0193b21163da27ba9c2bbe88232f18542d1e2d75
Satwik <satwikpawar4901@gmail.com>
2026-08-23 03:04:33 +0530

    Day 06 : MultipleOf

---
+++ src/day06/MultipleOfSeven.java
@@ -0,0 +1,14 @@
+package day06;
+
+public class MultipleOfSeven {
+    public static void main (String[] args){
+
+        int num = 44;
+
+        if(num % 7 == 0 ){
+            System.out.println("Number is multiple of 7");
+        }else{
+            System.out.println("Number is not multiple of 7");
+        }
+    }
+}
