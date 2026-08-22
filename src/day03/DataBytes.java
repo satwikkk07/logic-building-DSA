4887ec87f417cf75bb9b40a8f60ead6ed96a61d4
Satwik <satwikpawar4901@gmail.com>
2026-08-23 01:37:40 +0530

    Day 04 : Integer O/P

--- src/day03/DataBytes.java
+++ src/day03/DataBytes.java
@@ -5,8 +5,8 @@ public class DataBytes {
         // Divide by 8 to convert bits to bytes
         System.out.println("Size of int: " + (Integer.SIZE / 8) + " bytes"); // 32 bits/8 = 4bytes Soo
         System.out.println("Size of float: " + (Float.SIZE / 8) + " bytes");
-        System.out.println("Size of double: " + (Double.SIZE / 8) + " bytes");
-        System.out.println("Size of char: " + (Character.SIZE / 8) + " bytes");
+        System.out.println("Size of double: " + (Double.SIZE ) + " bits"); //8 bytes
+        System.out.println("Size of char: " + (Character.SIZE ) + " bits"); // 2 bytes
     }
 }
 
---
+++ src/day04/PrintInt.java
@@ -0,0 +1,13 @@
+package day04;
+
+import java.util.Scanner;
+
+public class PrintInt{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println("Entered number is " + num);
+    }
+}
