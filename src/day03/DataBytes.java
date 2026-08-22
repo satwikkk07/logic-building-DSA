d842c72c0dc3294c6aea692322467a83fefd655a
Satwik <satwikpawar4901@gmail.com>
2026-08-23 01:40:09 +0530

    Day 03 : Data Bytes

--- src/day03/DataBytes.java
+++ src/day03/DataBytes.java
@@ -6,7 +6,7 @@ public class DataBytes {
         System.out.println("Size of int: " + (Integer.SIZE / 8) + " bytes"); // 32 bits/8 = 4bytes Soo
         System.out.println("Size of float: " + (Float.SIZE / 8) + " bytes");
         System.out.println("Size of double: " + (Double.SIZE ) + " bits"); //8 bytes
-        System.out.println("Size of char: " + (Character.SIZE ) + " bits"); // 2 bytes
+        System.out.println("Size of char: " + (Character.SIZE ) + " bits Donee"); // 2 bytes not converting
     }
 }
 
