70f637b5caeb1f15907f75ea24ee59028ae30f2f
Satwik <satwikpawar4901@gmail.com>
2026-08-23 01:02:22 +0530

    Day 02: LargestOfTwo

---
+++ src/day02/LargestOfTwo.java
@@ -0,0 +1,20 @@
+package day02;
+
+import java.util.Scanner;
+
+public class LargestOfTwo {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if(num1 > num2){
+            System.out.println("Largest number is " + num1);
+        }
+        else{
+            System.out.println("Largest number is" + num2);
+        }
+
+    }
+}
