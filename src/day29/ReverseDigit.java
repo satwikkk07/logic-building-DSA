e174bde155914503af1bc53b391fab9adc0c45ec
Satwik <satwikpawar4901@gmail.com>
2026-09-15 00:50:12 +0530

    Day 28: DIGIT TO WORDS Reverse Str

---
+++ src/day29/ReverseDigit.java
@@ -0,0 +1,54 @@
+package day29;
+
+import java.util.Scanner;
+
+public class ReverseDigit {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            switch (digit) {
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
+            }
+
+            num = num / 10;
+        }
+
+        sc.close();
+    }
+}
