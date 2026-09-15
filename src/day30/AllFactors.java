f7769a16c42c56f0cd6aa8f96c6b20806d24aa7a
Satwik <satwikpawar4901@gmail.com>
2026-09-15 20:38:21 +0530

    Day 30: All Factors Of Num

---
+++ src/day30/AllFactors.java
@@ -0,0 +1,26 @@
+package day30;
+
+import java.util.Scanner;
+
+public class AllFactors {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+
+        int num = sc.nextInt();
+
+        System.out.print("Factors of "+num+" are : ");
+
+        for(int i =1; i <= num; i++){
+
+            if (num % i == 0) {
+                System.out.print( i + " ");
+
+
+            }
+        }
+        sc.close();
+    }
+}
