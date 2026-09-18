47b235584dacce64deb8dc7be2de235d74548bd1
Satwik <satwikpawar4901@gmail.com>
2026-09-19 01:46:36 +0530

    Day 33: Patterns Printing Shortcut Box

---
+++ src/day33/Pattern_printing.java
@@ -0,0 +1,30 @@
+package day33;
+
+ /*
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ */
+
+public class Pattern_printing {
+    public static void main(String[] args) {
+
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i + j <= n + 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+
