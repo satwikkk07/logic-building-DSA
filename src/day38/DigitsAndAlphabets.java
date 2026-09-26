473e1d7eb7677084b0db1131e0d1bc9cfcc5f055
Satwik <satwikpawar4901@gmail.com>
2026-09-26 17:54:41 +0530

    Day 38: Digits AND Alphabets

--- src/day38/DigitsAndAlphabets.java
+++ src/day38/DigitsAndAlphabets.java
@@ -17,6 +17,7 @@ public class DigitsAndAlphabets {
         for (char ch = 'A'; ch <= 'Z'; ch++) {
             System.out.print(ch + " ");
         }
+        System.out.println();
 
         for (char ch = 'a'; ch <= 'z'; ch++) {
             System.out.print(ch + " ");
